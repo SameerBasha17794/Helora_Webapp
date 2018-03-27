25ab788d7ae4493ee2e664827c9eb1f0ab727d3d
prakhar trivedi <prakhar trivedi>
2018-03-28 00:40:11 +0530

    Added constants file and API support.

---
+++ controller/constants.js
@@ -0,0 +1,39 @@
+/*
+Author : Abhay Gupta
+Description : This file consists of all the constant values which will
+be available for all the controllers
+*/
+
+//Variables
+
+var domain = "http://api.healora.com";
+var domainurl = "http://localhost/webapp_angular/";
+var purposePageCookieName = 'dr_insta_purpose_page_cookies';
+
+var psychologyCookie = 'psy_cookie';
+var dietCookie = 'diet_cookie';
+var dietExerciseCookie = 'diet_exer_cookie';
+var psychologyExercise1Cookie = 'psy_exer1_cookie';
+var psychologyExercise2Cookie = 'psy_exer2_cookie';
+var kunal = 'kunal';
+
+var medicalCookie = "medical_purpose";
+var PHONE_NUMBER_ALREADY_EXISTS = "Phone number already exists";
+var PROFILE_UPDATED = "Profile successfully updated";
+var PROFILE_DISPLAY_FAILED = "Failed to fetch profile information";
+var SOMETHING_WENT_WRONG = "Oops! Something went wrong";
+var CHILD_UPDATED = "Child updated successfully";
+//URLs
+var getListing = domain +"/product/getListing/";
+
+
+//set amount for payworld
+var basePrice=100;
+var taxPrice=15;
+var totalPrice=115;
+
+
+//set amount for payworld
+var basePriceaisect=100;
+var taxPriceaisect=15;
+var totalPriceaisect=115;
