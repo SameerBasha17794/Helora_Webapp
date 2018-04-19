c11c1d7a201ad685fa012ed965c00d27b10818e4
Kunal <kunal@simplifyreality.com>
2018-04-19 00:41:55 -0400

    test

--- controller/constants.js
+++ controller/constants.js
@@ -8,8 +8,8 @@ be available for all the controllers
 
 // var domain = "http://api.healora.com";
 // var domain = "http://127.0.0.1:5000";
- var domainurl = "http://front.healora.com";
- var domain = "http://api.healora.com";
+ var domainurl = "https://healora.com";
+ var domain = "https://api.healora.com";
 //var domainurl = "http://localhost/healora_webapp";
 var purposePageCookieName = 'dr_insta_purpose_page_cookies';
 
