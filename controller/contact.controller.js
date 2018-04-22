6547db49e34ecfab094a965d4f47c00e47fd49d5
Kunal <kunal@simplifyreality.com>
2018-04-21 23:06:57 -0400

    fsadf

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -4,24 +4,32 @@
         .module('app')
         .controller('ContactController', ContactController);
 
-    ContactController.$inject = ['$cookieStore','UserService', '$rootScope','$scope','$http','$location'];
-    function ContactController($cookieStore,UserService, $rootScope, $scope,$http,$location) {
+    ContactController.$inject = ['$cookieStore','UserService', '$rootScope','$scope','$http','$location','FlashService'];
+    function ContactController($cookieStore,UserService, $rootScope, $scope,$http,$location,FlashService) {
         var vm = this;
-        initController();
-
-        function initController() {
-        }
-
-        function loadCurrentUser() {
-            UserService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
-                    
-        
-            });
-        }
+        vm.instant = instant;
        
-        $scope.instant = function() {
-        }
+       function instant() {
+           var param = JSON.stringify({"email":vm.email, "page":"Contact page","subject":"Contact Page Submit", "data":{
+                                                "email":vm.email,
+                                                "name":vm.name,
+                                                "message":vm.message
+                                                }});
+                $http({
+                    url: sendAllEmail,
+                    method: "POST",
+                    data: param,
+                    headers: { 'Content-Type': 'application/json' }
+                }).success(function (data, status, headers, config) {
+                    FlashService.Success("Thanks. We will get back to you soon.");
+                    vm.email="";
+                    vm.name="";
+                    vm.message="";
+                }).error(function (data, status, headers, config) {
+                    FlashService.Error("Something went wrong. Please try again");
+                });
+       }
+    
         
     }
 
